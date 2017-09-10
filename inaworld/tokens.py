ae4f43790cbd0dfc0cd945f331e0dfafb92ed3bf
Eric Pfahl <epfahl@Erics-MacBook-Pro-2.local>
2017-09-10 10:03:08 -0400

    complete, but not fully tested tokenization script

--- inaworld/tokens.py
+++ inaworld/tokens.py
@@ -1,15 +1,48 @@
+"""Tokenize a document.
+"""
+
 import re
 import nltk
 import toolz as tz
 
+re_not_alpha = re.compile('[^a-zA-Z]')
+STOPWORDS = set(nltk.corpus.stopwords.words('english'))
 
-def tokenizer(*args):
-    """Return a funciton that tokenizes a document.
+
+def not_alpha(tt):
+    """Given a POS tagged token (<token>, <pos>), return True if the token has
+    only alphabetic characters (i.e., no punctuation or numbers).
     """
+    return not bool(re_not_alpha.search(tt[0]))
 
-    def tokenize(doc):
-        """Given a document string, return a list of tokens.
-        """
-        pass
 
-    return tokenize
+def not_proper(tt):
+    """Given a POS tagged token (<token>, <pos>), return True if the token is
+    not tagged as a proper noun ('NNP').
+    """
+    return (tt[1] != 'NNP')
+
+
+def not_stopword(tt):
+    """Given a POS tagged token (<token>, <pos>), return True if the token is
+    not a stopword.
+    """
+    return (tt[0] not in STOPWORDS)
+
+
+def lower(tt):
+    """Given a POS tagged token (<token>, <pos>), return
+    (<token>.lower(), <pos>)
+    """
+    return (tt[0].lower(), tt[1])
+
+
+def tokenize(doc):
+    """Given a document string, return a list of tokens.
+    """
+    return list(tz.thread_last(
+        nltk.tag.pos_tag(nltk.tokenize.word_tokenize(doc)),
+        (filter, not_alpha),
+        (filter, not_proper),
+        (map, lower),
+        (filter, not_stopword)))
