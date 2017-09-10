4009a61bd69a2fb89f7821097e3e918dae23dfb5
Eric Pfahl <epfahl@Erics-MacBook-Pro-2.local>
2017-09-10 09:43:39 -0400

    document tokenization module

---
+++ inaworld/tokens.py
@@ -0,0 +1,15 @@
+import re
+import nltk
+import toolz as tz
+
+
+def tokenizer(*args):
+    """Return a funciton that tokenizes a document.
+    """
+
+    def tokenize(doc):
+        """Given a document string, return a list of tokens.
+        """
+        pass
+
+    return tokenize
