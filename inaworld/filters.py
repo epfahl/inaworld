3c6732a3d42025a181a2125cb8583e6c9a44ece2
Eric Pfahl <epfahl@MacBook-Pro.local>
2017-09-17 17:44:57 -0400

    return a dict of boolean filter arrays for genres and movies

--- inaworld/filters.py
+++ inaworld/filters.py
@@ -12,7 +12,7 @@ def genre_counts(gv):
     return np.array(gv.sum(axis=0))[0]
 
 
-def genres(gc, min_count):
+def _genre_filter(gc, min_count):
     """Given an array of genre token counts, return a boolean array that
     indicates genres with counts greater than or equal to a given minimum
     count.
@@ -20,9 +20,26 @@ def genres(gc, min_count):
     return (gc >= min_count)
 
 
-def movies(gv, gf):
+def _movie_filter(gv, gf):
     """Given the matrix of genre vectors and a boolean genre filter array,
     return a boolean filter array that indicates which movies have at least one
     genre after application of the genre filter.
     """
     return (np.array(gv[:, gf].sum(axis=1)) > 0).reshape(-1,)
+
+
+def genres_and_movies(gv, min_genre_count):
+    """Given a matrix of genre vectors, return boolean filter arrays that
+    indicate which genres have at least a threshold count across the corpus,
+    and which movies have at least one genre with at least the threshold count.
+
+    Returns
+    -------
+    {
+        'genres': <boolean array to filter genres>,
+        'movies': <boolean array to filter movies>
+    }
+    """
+    gf = _genre_filter(genre_counts(gv), min_genre_count)
+    mf = _movie_filter(gv, gf)
+    return {'genres': gf, 'movies': mf}
