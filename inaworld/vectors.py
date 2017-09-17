37bf83cf1b8abee07683d9f6e3914087b2b4fb8d
Eric Pfahl <epfahl@MacBook-Pro.local>
2017-09-16 20:43:49 -0400

    sklearn-based genre and summary vectorization functions

---
+++ inaworld/vectors.py
@@ -0,0 +1,53 @@
+"""Functions to vectorize movie genres and summaries.
+
+Notes
+-----
+* Genres for a given movie are vectorized as a binary array with a length equal
+  to the total number of genres, where 1 indicates the presence of the genre.
+* Summaries are tokenized such that tokens have no non-alphabetic characters
+  (numbers, punctuation, etc.).
+* Summaries are vectorized using the unigram bag-of-words model and the
+  TF-IDF (term frequency, inverse document frequency) weighting for each token.
+"""
+
+import re
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+SUMMARY_TOKEN_PATTERN = r"\b[a-zA-Z]+\b"
+MAX_DF = 0.25
+
+
+def genres_tokenizer(genres):
+    """Given genres as a monolithic string of comma-separated genre tokens,
+    return a list of genre tokens.
+
+    Examples
+    --------
+    >>> genres_tokenizer('["Action", "Space western"]')
+    ['Action', 'Space western']
+    """
+    return re.sub('[\[\]\"]', '', genres).split(', ')
+
+
+def genres(gnr):
+    """Given an array of unprocessed genre strings, return (genre tokens array,
+    boolean indicator sparse matrix).
+    """
+    vectorizer = CountVectorizer(binary=True, tokenizer=genres_tokenizer)
+    return (
+        np.array(vectorizer.get_feature_names()),
+        vectorizer.fit_transform(gnr))
+
+
+def summaries(smr, max_df=MAX_DF):
+    """Given an array of unprocessed summary strings, return (summary tokens
+    array, TF-IDF sparse matrix).
+    """
+    vectorizer = TfidfVectorizer(
+        max_df=max_df,
+        token_pattern=SUMMARY_TOKEN_PATTERN,
+        stop_words='english')
+    return (
+        np.array(vectorizer.get_feature_names()),
+        vectorizer.fit_transform(smr))
