7f6aaaeae71b2a8fda2b31c7cd75d0d7e0aae562
Eric Pfahl <epfahl@MacBook-Pro.local>
2017-09-17 10:00:34 -0400

    add vectorize_and_filter to return ready-to-train data

--- inaworld/inaworld.py
+++ inaworld/inaworld.py
@@ -4,8 +4,11 @@
 import pandas as pd
 
 from . import utils
+from . import vectors
+from . import filters
 
 DEFAULT_DATA_PATH = 'movie_data.csv'
+MIN_GENRE_COUNT = 2
 
 
 def filter_summaries_genres(df):
@@ -17,7 +20,7 @@ def filter_summaries_genres(df):
         (df['summary'].str.len() > 0)]
 
 
-def load_summaries_genres(path):
+def load_summaries_genres(path=None):
     """Load movie data and return (summary string array, genre string array).
     The rows are filtered to remove entries with empty genre lists and
     summaries, but the data is otherwise unprocessed.
@@ -31,3 +34,39 @@ def load_summaries_genres(path):
         'runtime', 'box_office_revenue']
     df = filter_summaries_genres(pd.read_csv(path).drop(drop_cols, axis=1))
     return (df['summary'].values, df['genres'].values)
+
+
+def vectorize_and_filter(smrs, gnrs, min_genre_count=MIN_GENRE_COUNT):
+    """Given arrays of unprocessed summary and genre strings, return filtered
+    arrays of tokens and vectors for summaries and genres.  Filtered is done to
+    remove uncommon genres and movies that contain only uncommon genres.
+
+    Returns
+    -------
+
+    {
+        'summaries':
+            {
+                'tokens': <array of summary tokens>,
+                'vectors': <sparse matrix of filtered summary vectors>
+            },
+        'genres':
+            {
+                'tokens': <array of filtered genre tokens>,
+                'vectors': <sparse matrix of filtered genre vectors>
+            }
+    }
+
+    Note
+    ----
+    * This feels a bit clunkly (i.e., procedural) because the genre and movie
+      filters are built upon the genre vectorization and then applied to the
+      genre and summary vectors.
+    """
+    gt, gv = vectors.genres(gnrs)
+    gf = filters.genres(filters.genre_counts(gv), min_genre_count)
+    mf = filters.movies(gv, gf)
+    st, sv = vectors.summaries(smrs[mf])
+    return {
+        'summaries': {'tokens': st, 'vectors': sv},
+        'genres': {'tokens': gt[gf], 'vectors': gv[:, gf][mf, :]}}
