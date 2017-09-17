4525624db0b77ff61c0f66ea687c1a881e8936ca
Eric Pfahl <epfahl@MacBook-Pro.local>
2017-09-17 09:21:19 -0400

    add loading/filtering functions

--- inaworld/inaworld.py
+++ inaworld/inaworld.py
@@ -1,28 +1,33 @@
 """Main driving script.
 """
 
-# import toolz as tz
-import multiprocessing
+import pandas as pd
 
-from . import tokens
-from . import data
+from . import utils
 
-
-def load_genres_summaries(path=None):
-    return data.load(path=path)
+DEFAULT_DATA_PATH = 'movie_data.csv'
 
 
-def _tokenize(gs, with_stem=False):
-    """Given a dict of genres and summary, return a dict with summary replaced
-    by tokens.
+def filter_summaries_genres(df):
+    """Given a DataFrame of movie genres and summaries, filter rows according
+    to the minimum lengths of the genre and summary strings.
     """
-    return {
-        'genres': gs['genres'],
-        'summary': tokens.tokenize(gs['summary'], with_stem=with_stem)}
+    return df[
+        (df['genres'].str.len() > 2) &
+        (df['summary'].str.len() > 0)]
 
 
-def load_genres_tokens(genres_summaries):
-    """Given {'genres': List String, 'summary': String}, return
-    {'genres': List String, 'summary': List String}.
+def load_summaries_genres(path):
+    """Load movie data and return (summary string array, genre string array).
+    The rows are filtered to remove entries with empty genre lists and
+    summaries, but the data is otherwise unprocessed.
     """
-    return multiprocessing.Pool().map(_tokenize, genres_summaries)
+
+    if path is None:
+        path = utils.local_filepath(DEFAULT_DATA_PATH)
+
+    drop_cols = [
+        'id', 'title', 'release_date',
+        'runtime', 'box_office_revenue']
+    df = filter_summaries_genres(pd.read_csv(path).drop(drop_cols, axis=1))
+    return (df['summary'].values, df['genres'].values)
