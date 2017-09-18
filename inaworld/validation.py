dfffe07a949f3c228975b103e8e5fdd967273938
Eric Pfahl <epfahl@MacBook-Pro.local>
2017-09-17 22:29:47 -0400

    add classification performance report function

--- inaworld/validation.py
+++ inaworld/validation.py
@@ -1,3 +1,14 @@
 """Functions to assess classifier performance and to spot check specific
 input/output pairs.
 """
+
+from sklearn.metrics import classification_report
+
+
+def report(clf, x_test, y_test, tokens):
+    """Given a trained classifier with a predict method, input and output test
+    data, and a tokens that are index-aligned with the columns of the output
+    test data, return a string report of the classification perforamnce.
+    """
+    y_pred = clf.predict(x_test)
+    return classification_report(y_test, y_pred, target_names=tokens)
